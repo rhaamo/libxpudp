aff8389fa18b94f0edad3dd90940ab7401b8192d
Marc Lagrange <rhaamo@gruik.at>
2010-12-04 15:38:52 +0100

    Add xp_sender header with Packet Value Type struct

---
+++ xp_sender.h
@@ -0,0 +1,21 @@
+/* xp_sender.h
+ * This file is part of libxpudp
+ */
+
+/*!
+  @addtogroup xp_sender
+  @{
+ */
+
+#ifndef	__XP_SENDER_H
+#define	__XP_SENDER_H
+
+/*! \typedef PVALUE_TYPE */
+typedef enum _PVALUE_TYPE {
+	PVALUE_FLOAT = 0,	/*!< Packet value is float */
+	PVALUE_INT = 1		/*!< Packet value is integer */
+} PVALUE_TYPE;
+
+#endif /* __XP_SENDER_H */
+
+/*! @} */
